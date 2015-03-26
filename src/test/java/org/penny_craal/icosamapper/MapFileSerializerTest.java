d67f005445a6f3aef57f6674f92d7c5ba6fe83ec
Zamess <james.pearce@hotmail.com>
2015-03-26 14:33:33 +0200

    broke it

--- src/test/java/org/penny_craal/icosamapper/MapFileSerializerTest.java
+++ src/test/java/org/penny_craal/icosamapper/MapFileSerializerTest.java
@@ -59,6 +59,6 @@ public class MapFileSerializerTest {
         MapFileSerializer ms = new MapFileSerializer(f);
         ms.save(map);
         Map map2 = ms.load();
-        assertEquals(map2, map);
+        assertEquals(false, map);
     }
 }
